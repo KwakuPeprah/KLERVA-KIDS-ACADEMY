ee2bddb420aad145bc44fe60daca34395bc01e80
Kwaku <kwakupeprah64@gmail.com>
2025-07-05 18:46:34 +0000

    first commit

---
+++ KLERVA KIDS/KLERVA KIDS/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace KLERVA_KIDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.comboGender = new System.Windows.Forms.ComboBox();
+            this.dateDOB = new System.Windows.Forms.ComboBox();
+            this.comboNationality = new System.Windows.Forms.ComboBox();
+            this.comboBatch = new System.Windows.Forms.ComboBox();
+            this.comboLevel = new System.Windows.Forms.ComboBox();
+            this.comboHealth = new System.Windows.Forms.ComboBox();
+            this.dateRegistered = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.butDelete = new System.Windows.Forms.Button();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(682, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KLERVA Student Registration ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(342, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student_ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(342, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(342, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nationality";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(342, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Birth Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(342, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gender";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(342, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Full_Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(342, 401);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(151, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Date Registered";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(342, 335);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Batch";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(342, 367);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Health Status";
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(548, 113);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(290, 22);
+            this.txtStudentId.TabIndex = 10;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(548, 141);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(290, 22);
+            this.txtFullName.TabIndex = 11;
+            this.txtFullName.TextChanged += new System.EventHandler(this.txtFullName_TextChanged);
+            // 
+            // comboGender
+            // 
+            this.comboGender.FormattingEnabled = true;
+            this.comboGender.Location = new System.Drawing.Point(548, 185);
+            this.comboGender.Name = "comboGender";
+            this.comboGender.Size = new System.Drawing.Size(144, 24);
+            this.comboGender.TabIndex = 13;
+            this.comboGender.SelectedIndexChanged += new System.EventHandler(this.comboGender_SelectedIndexChanged);
+            // 
+            // dateDOB
+            // 
+            this.dateDOB.FormattingEnabled = true;
+            this.dateDOB.Location = new System.Drawing.Point(548, 223);
+            this.dateDOB.Name = "dateDOB";
+            this.dateDOB.Size = new System.Drawing.Size(226, 24);
+            this.dateDOB.TabIndex = 14;
+            this.dateDOB.SelectedIndexChanged += new System.EventHandler(this.dateDOB_SelectedIndexChanged);
+            // 
+            // comboNationality
+            // 
+            this.comboNationality.FormattingEnabled = true;
+            this.comboNationality.Location = new System.Drawing.Point(548, 261);
+            this.comboNationality.Name = "comboNationality";
+            this.comboNationality.Size = new System.Drawing.Size(226, 24);
+            this.comboNationality.TabIndex = 15;
+            this.comboNationality.SelectedIndexChanged += new System.EventHandler(this.comboNationality_SelectedIndexChanged);
+            // 
+            // comboBatch
+            // 
+            this.comboBatch.FormattingEnabled = true;
+            this.comboBatch.Location = new System.Drawing.Point(548, 335);
+            this.comboBatch.Name = "comboBatch";
+            this.comboBatch.Size = new System.Drawing.Size(144, 24);
+            this.comboBatch.TabIndex = 16;
+            // 
+            // comboLevel
+            // 
+            this.comboLevel.FormattingEnabled = true;
+            this.comboLevel.Location = new System.Drawing.Point(548, 301);
+            this.comboLevel.Name = "comboLevel";
+            this.comboLevel.Size = new System.Drawing.Size(144, 24);
+            this.comboLevel.TabIndex = 17;
+            // 
+            // comboHealth
+            // 
+            this.comboHealth.FormattingEnabled = true;
+            this.comboHealth.Location = new System.Drawing.Point(548, 365);
+            this.comboHealth.Name = "comboHealth";
+            this.comboHealth.Size = new System.Drawing.Size(144, 24);
+            this.comboHealth.TabIndex = 18;
+            // 
+            // dateRegistered
+            // 
+            this.dateRegistered.FormattingEnabled = true;
+            this.dateRegistered.Location = new System.Drawing.Point(548, 401);
+            this.dateRegistered.Name = "dateRegistered";
+            this.dateRegistered.Size = new System.Drawing.Size(226, 24);
+            this.dateRegistered.TabIndex = 19;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(207, 463);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(149, 81);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(387, 564);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 81);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Report";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // butDelete
+            // 
+            this.butDelete.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.butDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butDelete.Location = new System.Drawing.Point(751, 463);
+            this.butDelete.Name = "butDelete";
+            this.butDelete.Size = new System.Drawing.Size(149, 81);
+            this.butDelete.TabIndex = 23;
+            this.butDelete.Text = "Delete";
+            this.butDelete.UseVisualStyleBackColor = false;
+            // 
+            // butSearch
+            // 
+            this.butSearch.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.butSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butSearch.Location = new System.Drawing.Point(574, 463);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(149, 81);
+            this.butSearch.TabIndex = 24;
+            this.butSearch.Text = "Search";
+            this.butSearch.UseVisualStyleBackColor = false;
+            this.butSearch.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(574, 564);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(149, 81);
+            this.button6.TabIndex = 25;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(387, 463);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(149, 81);
+            this.btnUpdate.TabIndex = 26;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 669);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.butDelete);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dateRegistered);
+            this.Controls.Add(this.comboHealth);
+            this.Controls.Add(this.comboLevel);
+            this.Controls.Add(this.comboBatch);
+            this.Controls.Add(this.comboNationality);
+            this.Controls.Add(this.dateDOB);
+            this.Controls.Add(this.comboGender);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KLERVA KIDS Registration Portal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.ComboBox comboGender;
+        private System.Windows.Forms.ComboBox dateDOB;
+        private System.Windows.Forms.ComboBox comboNationality;
+        private System.Windows.Forms.ComboBox comboBatch;
+        private System.Windows.Forms.ComboBox comboLevel;
+        private System.Windows.Forms.ComboBox comboHealth;
+        private System.Windows.Forms.ComboBox dateRegistered;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button butDelete;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ KLERVA KIDS/KLERVA KIDS/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace KLERVA_KIDS
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFormControls();
+        }
+
+        private void InitializeFormControls()
+        {
+            //Set dropdown values
+            comboGender.Items.AddRange(new string[] { "Male", "Female" });
+            comboLevel.Items.AddRange(new string[] { "100", "200", "300", "400" });
+            comboHealth.Items.AddRange(new string[] { "NILL", "SICK" });
+            comboNationality.Items.AddRange(new string[] { "Ghanaian", "Japaneese", "American", "British", "Others" });
+            comboBatch.Items.AddRange(new string[] { "7-9 AM", "9 - 11 AM", "11 - 1 PM", "1 - 3 PM" });
+        }
+
+        private void txtFullName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateDOB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboNationality_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Students_Master_Table (Full_Name, Gender, Date_Of_Birth, Nationality, Level, Batch, Health_Status, Date_Registerd) VALUES (@FullName, @Gender, @DOB, @Nationality, @Level, @Batch, @HealthStatus, @DateReg)";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@FullName", txtFullName.Text);
+                cmd.Parameters.AddWithValue("@Gender", comboGender.Text);
+                cmd.Parameters.AddWithValue("DOB", dateDOB.Text);
+                cmd.Parameters.AddWithValue("@Nationality", comboNationality.Text);
+                cmd.Parameters.AddWithValue("@Level", comboLevel.Text);
+                cmd.Parameters.AddWithValue("@Batch", comboBatch.Text);
+                cmd.Parameters.AddWithValue("@HealthStatus", comboHealth.Text);
+                cmd.Parameters.AddWithValue("@DateReg", dateRegistered.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Student record saved successfully.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE Students_Master_Table SET Full_Name=@FullName, Gender=@Gender, Date_Of_Birth=@DOB, Nationality=@Nationality, Level=@Level, Batch=@Batch, Health_Status=@HealthStatus, Date_Registered=DateReg WHERE Student_Id=@Id";
+
+            using (SqlCommand cmd = new SqlCommand(@query, conn))
+            {
+                cmd.Parameters.AddWithValue("@FullName", txtFullName.Text);
+                cmd.Parameters.AddWithValue("@Gender", comboGender.Text);
+                cmd.Parameters.AddWithValue("DOB", dateDOB.Text);
+                cmd.Parameters.AddWithValue("@Nationality", comboNationality.Text);
+                cmd.Parameters.AddWithValue("@Level", comboLevel.Text);
+                cmd.Parameters.AddWithValue("@Batch", comboBatch.Text);
+                cmd.Parameters.AddWithValue("@HealthStatus", comboHealth.Text);
+                cmd.Parameters.AddWithValue("@Id", txtStudentId.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Student record updated");
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Students_Master_Table WHERE Student_Id=@Id";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+
+            }
+        }
+    }
+}
